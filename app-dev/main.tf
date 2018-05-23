30abcf07f46f498517dc5a8c20b819a591759e57
apwickrama <apwickrama@gmail.com>
2018-05-23 14:39:17 -0500

    removed mandatory tags.

--- app-dev/main.tf
+++ app-dev/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_resource_group" "module" {
   location = "${var.location}"
 
   tags {
-    environment = "dev"
-    owner       = "me"
+    env = "dev"
+    own       = "me"
   }
 }
